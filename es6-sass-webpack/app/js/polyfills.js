2a3a143151fef3dc1c69f6ef5ab42712c131eccb
Hay Kranen <huskyr@gmail.com>
2018-02-28 19:43:15 +0100

    Initial commit

---
+++ es6-sass-webpack/app/js/polyfills.js
@@ -0,0 +1,13 @@
+// This is a sane selection of polyfills that support at least IE11
+import fastclick from 'fastclick';
+import 'es6-promise/auto';
+import 'whatwg-fetch';
+import 'intersection-observer';
+import 'mdn-polyfills/Array.from';
+import 'mdn-polyfills/Array.prototype.includes';
+import 'mdn-polyfills/Element.prototype.matches';
+import 'mdn-polyfills/Object.assign';
+import 'mdn-polyfills/String.prototype.includes';
+import 'mdn-polyfills/String.prototype.repeat';
+
+fastclick(document.body);
