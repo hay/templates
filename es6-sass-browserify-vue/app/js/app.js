398893929c118b443cd8f4b7c432f12af4866f27
Hay Kranen <huskyr@gmail.com>
2018-05-09 17:20:16 +0200

    Initial setup

---
+++ es6-sass-browserify-vue/app/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import { test } from './test.js'
+import Test from './test.vue';
+
+async function main() {
+    test();
+}
+
+main();
---
+++ es6-sass-browserify-vue/app/js/test.js
@@ -0,0 +1,20 @@
+export async function test() {
+    const json = await fetch("data.json");
+    const data = await json.json();
+    const el = Array.from(document.querySelectorAll("main"));
+    console.log(el);
+    console.log([1,2,3].includes(2));
+    console.log(Object.assign({ a : 1}, {a : 2}));
+    console.log("hallo".includes("lo"));
+    console.log("hallo".repeat(5));
+
+    new Vue({
+        el : "main",
+
+        components : {
+            Test
+        },
+
+        data
+    });
+}
