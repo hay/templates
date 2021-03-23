8a910ada9e9619a4055f618977bc54ce673bac30
Hay Kranen <hay@haykranen.nl>
2021-03-23 21:57:34 +0100

    Adding first parts of a vue3 project

---
+++ vue3-cli-app-vuex-router/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <screen-home></screen-home>
+</template>
+
+<script>
+import ScreenHome from './components/screen-home.vue';
+
+export default {
+    components: {
+        ScreenHome
+    }
+}
+</script>
