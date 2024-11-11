32e9171b0f1438577754d66f3cafd78edc1b6752
Hay Kranen <hay@haykranen.nl>
2024-11-11 14:20:04 +0100

    Updating template

--- vue3-pinia/vite.config.js
+++ vue3-pinia/vite.config.js
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
+    css : {
+        preprocessorOptions : {
+            scss : {
+                api: 'modern-compiler'
+            }
+        }
+    },
     plugins: [
         vue(),
         removeConsole()
